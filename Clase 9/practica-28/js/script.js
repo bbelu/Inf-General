ba219297e2a631d75b777be68be20597d25adcc3
bbelu <belensoares@gmail.com>
2026-06-20 21:47:41 -0300

    Update script.js

---
+++ Clase 9/practica-28/js/script.js
@@ -0,0 +1,16 @@
+function organizarCena (){
+    let contPasta = 0 ;
+    let contPollo =  0 ;   
+    let contEnsalada= 0 ;
+
+    const totalInvitados = 12;
+
+    for (let i = 1; i<=totalInvitados; i++){
+        let eleccion = prompt("Invitado" + i+"¿Que menu elige?(pasta / pollo / ensalada)");
+
+    }
+    If ( eleccion=== null){
+        eleccion="";
+    }
+
+}
